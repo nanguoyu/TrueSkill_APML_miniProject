297844aa04476cf93c09cb0cb78fb913f37f68fc
Vevark <Vevark@users.noreply.github.com>
2020-09-30 04:07:52 +0800

    Update and rename main.py to runme.py

--- main.py
+++
@@ -1,5 +0,0 @@
-from QWD import Q9, Q56
-
-if __name__ == '__main__':
-    Q56()
-    Q9()
---
+++ runme.py
@@ -0,0 +1,13 @@
+from QWD import Q9, Q56
+
+if __name__ == '__main__':
+    
+    # In Question 4 we need first do Gibbs sampling with K
+    K = 500  # tune the number of samples
+    S1, S2, T, E_S1, E_S2, Var_S1, Var_S2, Var_T = Q4_Gibbs(K, 1)
+    # Then plot and save figures with suitble burn-in
+    burnInNum = 120  # tune the number of burn-in
+    Q4_plot(burnInNum, S1, S2, E_S1, E_S2, Var_S1, Var_S2)
+    
+    Q56()
+    Q9()
